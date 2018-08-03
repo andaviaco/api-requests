524c0b1239e76de3cd982093d630ff3a795e62d7
Michelle Torres <mtorres@tiempodevelopment.com>
2018-08-02 20:19:08 -0500

    Linter

--- js/fetch.js
+++ js/fetch.js
@@ -1,61 +1,60 @@
 async function getPopularProducts(){
     // Enviar la informacion al API
-	const myRequest = new Request(
-		'http://localhost:3000/products',
-		{
-			method: 'GET'
-		}
-	);
+    const myRequest = new Request(
+        'http://localhost:3000/products',
+        {
+            method: 'GET'
+        }
+    );
 
-	let response = await fetch(myRequest)
-	.then((response) => {
-		if (response.status === 200) {
-  			return response.json();
-    	} else {
-      		throw new Error('Something went wrong on api server!');
-    	}
-	})
+    let response = await fetch(myRequest)
     .then((response) => {
-		return response;
-  	}).catch(error => {
-    	console.error(error);
-  	});
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error('Something went wrong on api server!');
+        }
+    })
+    .then((response) => {
+        return response;
+    }).catch(error => {
+        console.error(error);
+    });
 
-	return response;
+    return response;
 }
 
 async function renderProducts(){
-	let products = await getPopularProducts();
-	console.log(products);
+    let products = await getPopularProducts();
+    console.log(products);
 
-	products.forEach((product) => {
-		console.log(product);
-		let popularProductsNode = document.getElementById('popular-products');
-		let articleNode = document.createElement('article');
-		let contentNode = document.createElement('div');
+    products.forEach((product) => {
+        console.log(product);
+        let popularProductsNode = document.getElementById('popular-products');
+        let articleNode = document.createElement('article');
+        let contentNode = document.createElement('div');
 
-		// Foto
-		let imageNode = document.createElement('img');
-		imageNode.src = 'img/' + product.photo;
-		imageNode.alt = product.name;
+        // Foto
+        let imageNode = document.createElement('img');
+        imageNode.src = 'img/' + product.photo;
+        imageNode.alt = product.name;
 
-		// Nombre
-		let titleNode = document.createElement('h2');
-		let textNode = document.createTextNode(product.name);
-		titleNode.appendChild(textNode);
+        // Nombre
+        let titleNode = document.createElement('h2');
+        let textNode = document.createTextNode(product.name);
+        titleNode.appendChild(textNode);
 
-		// Marca
-		let brandNode = document.createElement('p');
-		let brandTextNode = document.createTextNode(product.brand);
-		brandNode.appendChild(brandTextNode);
+        // Marca
+        let brandNode = document.createElement('p');
+        let brandTextNode = document.createTextNode(product.brand);
+        brandNode.appendChild(brandTextNode);
 
+        contentNode.appendChild(titleNode);
+        articleNode.appendChild(imageNode);
+        articleNode.appendChild(contentNode);
 
-		contentNode.appendChild(titleNode);
-		articleNode.appendChild(imageNode);
-		articleNode.appendChild(contentNode);
-
-		popularProductsNode.appendChild(articleNode);
-	});
+        popularProductsNode.appendChild(articleNode);
+    });
 }
 
 
